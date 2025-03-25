fc7605217e85b416e292d567a8894f6f161fc94f
ElhamAbedAlkareem <34744131+ElhamAbedAlkareem@users.noreply.github.com>
2025-03-26 01:18:34 +0200

    Updated Spacecheck.sh with the correct variables and fuctions name

--- Spacecheck.sh
+++ Spacecheck.sh
@@ -1,23 +1,12 @@
 #!/bin/bash 
+. ./MainScript.sh
 
-#variables:
-#LOG_FILE
-#DIRECTORY_MAX_SIZE
-#SOURCE_FILE
-#DESTINATION_PAATH
-#OPERATION
-#SOURCE_FILE_SIZE
-#DESTINATION_AVAILABLE_SPACE_BEFORE
-#DESTINATION_AVAILABLE_SPACE_AFTER
-#USER_SELECTED_RESOLUTION
-#OPERATION_PERFORM_STATUS
-
-#check srcoe file/dir size in bytes
-function check_source_size(){
+#check source file/dir size in bytes
+soure_file_validation(){
     SOURCE_FILE_SIZE=$(stat -c %s "$SOURCE_FILE")
 }       
 
 #check destination free space in bytes
-function check_destination_free_space(){
-    DESTINATION_FREE_SPACE_BEFORE=$(df -P $DESTINATION_PATH | awk 'NR==2 {print $4}')
+destination_path_validation(){
+    DESTINATION_AVAILABLE_SPACE_BEFORE=$(df -P $DESTINATION_PATH | awk 'NR==2 {print $4}')
 }
