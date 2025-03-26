c52d7927ff6f220ae8b44536e8d7c76a5fb43b68
ElhamAbedAlkareem <34744131+ElhamAbedAlkareem@users.noreply.github.com>
2025-03-26 06:16:35 +0200

    Update SpaceCheck.sh

--- SpaceCheck.sh
+++ SpaceCheck.sh
@@ -1,13 +1,14 @@
 #!/bin/bash 
 . ./MainScript.sh
 
-#check source file/dir size in Kbytes
-soure_file_validation(){
+#check source file/dir size in KB
+soure_file_size(){
 	SOURCE_FILE_SIZE=$(du -h $SOURCE_FILE)
 }       
 
-#check destination free space in Kbytes
-destination_path_validation(){
-    DESTINATION_AVAILABLE_SPACE_BEFORE=$(du -sh $DESTINATION_PATH)
+#check destination free space in KB
+destination_available_space(){
+    destination_size=$(du -sk "$DESTINATION_PATH" | awk '{print $1}')  # Get size in KB
+    DESTINATION_AVAILABLE_SPACE=$((DIRECTORY_MAX_SIZE - destination_size))
 }
 
