aa37ae99d304b41279c538a7c84b0453cfddc1de
Elham Abed Alkareem <elham.aa.91@gmail.com>
2025-03-26 00:14:38 +0000

    Modified the calculate size functions to get sizes in KB

--- InitialValidation.sh
+++ InitialValidation.sh
@@ -1,12 +1,13 @@
 #!/bin/bash 
 . ./MainScript.sh
 
-#check source file/dir size in bytes
+#check source file/dir size in Kbytes
 soure_file_validation(){
-    SOURCE_FILE_SIZE=$(stat -c %s "$SOURCE_FILE")
+	SOURCE_FILE_SIZE=$(du -h $SOURCE_FILE)
 }       
 
-#check destination free space in bytes
+#check destination free space in Kbytes
 destination_path_validation(){
-    DESTINATION_AVAILABLE_SPACE_BEFORE=$(df -P $DESTINATION_PATH | awk 'NR==2 {print $4}')
+    DESTINATION_AVAILABLE_SPACE_BEFORE=$(du -sh $DESTINATION_PATH)
 }
+
