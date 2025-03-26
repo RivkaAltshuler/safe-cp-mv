9df147f91258d03a45bb4517a461da6676833de6
ElhamAbedAlkareem <34744131+ElhamAbedAlkareem@users.noreply.github.com>
2025-03-26 10:00:28 +0200

    Update InitialValidation.sh

--- InitialValidation.sh
+++ InitialValidation.sh
@@ -4,7 +4,7 @@
 check_writable() {
     if [ ! -w $SOURCE_FILE ]; then
         Message="$(date +'%Y-%m-%d %H:%M:%S') - $SOURCE_FILE Error: Destination path is not writable."
-        error_logging $Message
+        error_logging "$Message"
         exit 1
     fi
 }
@@ -12,7 +12,7 @@ check_writable() {
 check_if_file_exist() {
     if [ ! -e $SOURCE_FILE ]; then
     	Message="$(date +'%Y-%m-%d %H:%M:%S') - $SOURCE_FILE "Error: Source path does not exist."
-        error_logging $Message
+        error_logging "$Message"
         exit 1
     fi
 }
