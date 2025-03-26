4bbe0985091a75739985909ee7cee9e33b494724
Elham Abed Alkareem <elham.aa.91@gmail.com>
2025-03-26 12:01:19 +0000

    Fixed Errors

--- InitialValidation.sh
+++ InitialValidation.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
+
 . ./Logging.sh
 
-check_writable() {
-    if [ ! -w $SOURCE_FILE ]; then
-        Message="$(date +'%Y-%m-%d %H:%M:%S') - $SOURCE_FILE Error: Destination path is not writable."
+check_writable(){
+
+	if [ ! -w "$DESTINATION_PATH" ]; then
+        Message="$DESTINATION_PATH : Destination path is not writable."
         error_logging "$Message"
         exit 1
-    fi
+   	fi
 }
 
-check_if_file_exist() {
-    if [ ! -e $SOURCE_FILE ]; then
-    	Message="$(date +'%Y-%m-%d %H:%M:%S') - $SOURCE_FILE "Error: Source path does not exist."
+check_if_file_exist(){
+
+        if [ ! -e "$SOURCE_FILE" ]; then
+   	Message="$SOURCE_FILE : Source path does not exist."
         error_logging "$Message"
         exit 1
-    fi
+        fi
 }
  
 
